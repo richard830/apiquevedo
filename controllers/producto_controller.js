e8153e39ac25278d44b9e0ea8fea6288ea17cd6a
Richard <74387852+richard830@users.noreply.github.com>
2024-04-15 00:25:35 -0500

    Update producto_controller.js

--- controllers/producto_controller.js
+++ controllers/producto_controller.js
@@ -45,10 +45,27 @@ module.exports = {
     },
 
 
+    async listarProductoIdSubCategoria(req, res, next) {
+        try {
+            const id = req.params.id_sub;
+            const lista = await Producto.getProductoidSubCategor(id);
+
+            return res.status(200).json(lista);
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(500).json({
+                success: false,
+                message: 'Error al obtener la lista de empresas'
+            });
+        }
+    },
+
+
     async listarProductoImagen(req, res, next) {
         try {
             const id = req.body;
             const lista = await Producto.listaProducto(id);
+
 
             return res.status(200).json(lista);
         } catch (error) {
