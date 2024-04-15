d8e80baa5232ccbee16f4edf98ca138330977d59
Richard <74387852+richard830@users.noreply.github.com>
2024-04-15 15:28:30 -0500

    Update sub_categoria_Routes.js

--- routes/sub_categoria_Routes.js
+++ routes/sub_categoria_Routes.js
@@ -6,7 +6,7 @@ const SubCategoriaController = require('../controllers/sub_categoria_controller'
 module.exports = (app, subirImagen) => {
 
 
-    app.post('/api/sub_categoria/subir_subcategoria', subirImagen.single('image_sub'), SubCategoriaController.subirSubCategoriaController);
+    app.post('/api/sub_categoria/subir_subcategoria', subirImagen.array('image_sub', 1), SubCategoriaController.subirSubCategoriaController);
     app.get('/api/sub_categoria/listarsubCategoria', SubCategoriaController.listarSubCategoria);
     app.get('/api/sub_categoria/listarsubCategoriaxID/:id_categoria', SubCategoriaController.listarSubCategoriaxID);
 
