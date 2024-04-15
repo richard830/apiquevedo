092f95920e16498cb6c9e9243b1fdd9fd4f2944f
Richard <74387852+richard830@users.noreply.github.com>
2024-04-15 00:26:03 -0500

    Update sub_categoria_controller.js

--- controllers/sub_categoria_controller.js
+++ controllers/sub_categoria_controller.js
@@ -8,8 +8,16 @@ module.exports = {
         try {
             const image = req.file;
             const datos = req.body;
-            //  const datos = JSON.parse(req.body);
+            const nombre_sub = req.body.nombre_sub;
+            const myuser = await SubCategoria.verificarExisteNombreSubCategoria(nombre_sub);
 
+            if (myuser) {
+                return res.status(401).json({
+                    success: false,
+                    message: 'La SubCategoria ya existe'
+                });
+
+            }
             data = await SubCategoria.subirSubCategoria(datos, image);
             console.log(data);
             return res.status(201).json({
@@ -38,6 +46,32 @@ module.exports = {
             const lista = await SubCategoria.listarSubCategoria(id);
 
             return res.status(200).json(lista);
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(500).json({
+                success: false,
+                message: 'Error al obtener la lista de empresas'
+            });
+        }
+    },
+
+
+
+    async listarSubCategoriaxID(req, res, next) {
+        try {
+            const id = req.params.id_categoria;
+            const lista = await SubCategoria.listarSubCategoriaID(id);
+            const listas = lista.map(producto => {
+                return {
+                    nombre_sub: producto.nombre_sub,
+                    image_sub: `http://192.168.0.100:3000/uploads/${producto.image_sub}`,
+                    id_categoria: producto.id_categoria,
+                    id_sub: producto.id_sub,
+
+                };
+            });
+
+            return res.status(200).json(listas);
         } catch (error) {
             console.log(`Error: ${error}`);
             return res.status(500).json({
