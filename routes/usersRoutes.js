21c896473d63b4900592869739986a7f242a9228
Richard <74387852+richard830@users.noreply.github.com>
2024-04-15 00:32:31 -0500

    Update usersRoutes.js

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -8,7 +8,7 @@ module.exports = (app, subirImagen) => {
     app.post('/api/users/crear', UsersController.Crear);
     // app.post('/api/users/crear', subirImagen.single('image'), UsersController.Crear);
     app.post('/api/users/login', UsersController.login);
-
+    app.post('/api/users/logout', UsersController.logout);
 
 
 
