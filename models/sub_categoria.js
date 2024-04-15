dcc227f5c874bf300086241e887e96ff9fcc393e
Richard <74387852+richard830@users.noreply.github.com>
2024-04-15 15:27:36 -0500

    Update sub_categoria.js

--- models/sub_categoria.js
+++ models/sub_categoria.js
@@ -4,7 +4,7 @@ const SubCategoria = {};
 
 
 
-SubCategoria.subirSubCategoria = (sub, image) => {
+SubCategoria.subirSubCategoria = (sub) => {
 
     const sql = `
     INSERT INTO subcategoria (nombre_sub, image_sub, id_categoria, fecha_sub)
@@ -12,7 +12,7 @@ SubCategoria.subirSubCategoria = (sub, image) => {
     RETURNING *`;
     return db.oneOrNone(sql, [
         sub.nombre_sub,
-        image.filename,
+        sub.image_sub,
         sub.id_categoria,
         new Date()
     ]);
@@ -31,10 +31,23 @@ SubCategoria.listarSubCategoriaID = (id_categoria) => {
     return db.manyOrNone(sql, id_categoria);
 };
 
-SubCategoria.verificarExisteNombreSubCategoria = (nombre) => {
-    const sql = `SELECT * FROM subcategoria where nombre_sub = $1;`;
-    return db.manyOrNone(sql, nombre);
+
+SubCategoria.verificarExisteNombreSubCategoria = async(nombre) => {
+    try {
+        const sql = `SELECT * FROM subcategoria where nombre_sub = $1;`;
+        const result = await db.manyOrNone(sql, nombre);
+        return result.length > 0; // Devuelve true si hay al menos un resultado, lo que significa que el nombre ya existe
+    } catch (error) {
+        console.error('Error al verificar si la categoría ya existe:', error);
+        throw error; // Propaga el error para que pueda ser manejado por el controlador
+    }
 };
+
+
+// SubCategoria.verificarExisteNombreSubCategoria = (nombre) => {
+//     const sql = `SELECT * FROM subcategoria where nombre_sub = $1;`;
+//     return db.manyOrNone(sql, nombre);
+// };
 
 
 module.exports = SubCategoria;
