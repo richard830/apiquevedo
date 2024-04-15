e6beae1aa825c4229949a3cb9634b2a22ab65213
Richard <74387852+richard830@users.noreply.github.com>
2024-04-15 00:24:46 -0500

    Update categoria_controller.js

--- controllers/categoria_controller.js
+++ controllers/categoria_controller.js
@@ -8,6 +8,16 @@ module.exports = {
         try {
             const image = req.file;
             const datos = req.body;
+            const nombre = req.body.nombre;
+            const myuser = await Categoria.verificarExisteNombreCategoria(nombre);
+
+            if (myuser) {
+                return res.status(401).json({
+                    success: false,
+                    message: 'La Categoria ya existe'
+                });
+
+            }
 
             data = await Categoria.subirCategoria(datos, image);
             console.log(data);
@@ -35,6 +45,20 @@ module.exports = {
         try {
             const id = req.body;
             const lista = await Categoria.listarCategoria(id);
+
+            return res.status(200).json(lista);
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(500).json({
+                success: false,
+                message: 'Error al obtener la lista de empresas'
+            });
+        }
+    },
+    async listarCategoriaLimites(req, res, next) {
+        try {
+            const id = req.body;
+            const lista = await Categoria.listarCategoriaLimite(id);
 
             return res.status(200).json(lista);
         } catch (error) {
