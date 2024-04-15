b4c1f8496dfc0406746f72bf41eaec1dcff8127d
Richard <74387852+richard830@users.noreply.github.com>
2024-04-15 00:31:56 -0500

    Update producto_Routes.js

--- routes/producto_Routes.js
+++ routes/producto_Routes.js
@@ -9,6 +9,7 @@ module.exports = (app, subirImagen) => {
     app.post('/api/producto/subir_producto', ProductoController.subirProdcutoController);
     app.get('/api/producto/listarProducto', ProductoController.listarProducto);
     app.get('/api/producto/listarProductoImagen', ProductoController.listarProductoImagen);
+    app.get('/api/producto/listarProductosubCategoria/:id_sub', ProductoController.listarProductoIdSubCategoria);
 
 
     // app.post('/api/producto/subir_producto', subirImagen.single('image'), CategoriaController.subirCategoriaController);
