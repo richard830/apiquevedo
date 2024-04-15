7b8cf6b7edc779f60652383455c17055203b7b2d
Richard <74387852+richard830@users.noreply.github.com>
2024-04-15 15:28:00 -0500

    Update categoria_Routes.js

--- routes/categoria_Routes.js
+++ routes/categoria_Routes.js
@@ -6,7 +6,8 @@ const CategoriaController = require('../controllers/categoria_controller');
 module.exports = (app, subirImagen) => {
 
 
-    app.post('/api/categoria/subir_categoria', subirImagen.single('image'), CategoriaController.subirCategoriaController);
+    app.post('/api/categoria/subir_categoria', subirImagen.array('image', 1), CategoriaController.subirCategoriaController);
+    // app.post('/api/categoria/subir_categoria', subirImagen.single('image'), CategoriaController.subirCategoriaController);
     app.get('/api/categoria/listarCategoria', CategoriaController.listarCategoria);
     app.get('/api/categoria/listarCategoriaLimite', CategoriaController.listarCategoriaLimites);
 
