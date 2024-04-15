f7a4cb8e2dbc9771617776a7e9e9f8ef8f0d60e4
Richard <74387852+richard830@users.noreply.github.com>
2024-04-15 15:28:15 -0500

    Update image_pro_Routes.js

--- routes/image_pro_Routes.js
+++ routes/image_pro_Routes.js
@@ -6,7 +6,7 @@ const ImageProController = require('../controllers/image_pro_controller');
 module.exports = (app, subirImagen) => {
 
 
-    app.post('/api/image/subir_image', subirImagen.single('image_pro'), ImageProController.subirImageController);
+    app.post('/api/image/subir_image', subirImagen.array('image_pro', 1), ImageProController.subirImageController);
 
     // app.get('/api/categoria/listarCategoria', CategoriaController.listarCategoria);
 
